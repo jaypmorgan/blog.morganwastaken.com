2e6889f68c0e6acc9545faf56b7597bfd34a6c93
Jay Morgan <jaymiles17@gmail.com>
2022-12-04 16:44:05 +0000

    Initial commit -- create blog & add existing articles

---
+++ publish.el
@@ -0,0 +1,28 @@
+(require 'ox-publish)
+
+(setq org-html-preamble-format
+      '(("en"
+         "<h1 class=\"title\">%t</h1>
+          <p class=\"subtitle\">%d</p>")))
+
+(setq org-publish-project-alist
+      '(("posts"
+	 :base-directory "./"
+	 :base-extension "org"
+	 :publishing-directory "./public/"
+;	 :exclude ".*/public/.*"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html)
+	("css"
+	 :base-directory "css/"
+	 :base-extension "css"
+	 :publishing-directory "public/css"
+	 :publishing-function org-publish-attachment)
+	("images"
+	 :base-directory "./"
+	 :base-extension "png\\|jpg\\jpeg"
+	 :recursive t
+;	 :exclude "./public/.*"
+	 :publishing-function org-publish-attachment
+	 :publishing-directory "./public/")
+	("all" :components ("posts" "css" "images"))))
